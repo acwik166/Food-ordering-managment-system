246f74de6a534c6e6830e155aa95842944a20a8d
acwik166 <maciekposala11@gmail.com>
2020-08-09 18:41:25 +0200

    basic frontend router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './routes/Home';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
