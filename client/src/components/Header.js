54a9462caf360c8aa8353870aa581b7d8afdd86a
acwik166 <maciekposala11@gmail.com>
2020-08-16 20:41:55 +0200

    hid .env file

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/AuthContext';
+
 const Header = () => {
+  const { isAuthenticated } = useContext(AuthContext);
   return (
     <header>
       <h1>Foodable</h1>
       <nav>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Register</Link></li>
+        { isAuthenticated ? 
+        <> 
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/logout">Logout</Link></li> :
+        </> :
+        <>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Register</Link></li>
+        </>
+        }
       </nav>
     </header>
   )
